59b73ec83262dc85f9b08e61460af9be38e454fd
Tiago Silva <tiago@bright42.com>
2018-02-23 21:36:16 -0700

    Convert ExampleInstrumentation test to Kotlin

---
+++ app/src/androidTest/java/com/sapuglha/tictactoe/ExampleInstrumentedTest.kt
@@ -0,0 +1,18 @@
+package com.sapuglha.tictactoe
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.sapuglha.tictactoe", appContext.packageName)
+    }
+}
