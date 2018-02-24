616e72275e7c32167143d9068638bab4f3a74d11
Tiago Silva <tiago@bright42.com>
2018-02-23 21:36:54 -0700

    Create Espresso test for valid game play

---
+++ app/src/androidTest/java/com/sapuglha/tictactoe/ui/MainActivityTest.kt
@@ -0,0 +1,40 @@
+package com.sapuglha.tictactoe.ui
+
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.test.suitebuilder.annotation.LargeTest
+import com.sapuglha.tictactoe.R
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun playerXWinsTest() {
+        onView(allOf(withId(R.id.activity_main_board_0_0), isDisplayed())).perform(click())
+
+        onView(allOf(withId(R.id.activity_main_board_1_0), isDisplayed())).perform(click())
+
+        onView(allOf(withId(R.id.activity_main_board_1_1), isDisplayed())).perform(click())
+
+        onView(allOf(withId(R.id.activity_main_board_2_1), isDisplayed())).perform(click())
+
+        onView(allOf(withId(R.id.activity_main_board_2_2), isDisplayed())).perform(click())
+
+        onView(allOf(withId(R.id.activity_main_label_winner), withText("X"), isDisplayed()))
+                .check(matches(withText("X")))
+
+    }
+}
