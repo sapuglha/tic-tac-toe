7389e78edff4dd6097763ab8b3c5cc8fff4a0a51
Tiago Silva <sapuglha@gmail.com>
2020-03-23 21:57:03 -0600

    Update project
    Move away from Kotlin DSL for android project

--- app/src/main/java/com/sapuglha/tictactoe/TttApp.kt
+++ app/src/main/java/com/sapuglha/tictactoe/TttApp.kt
@@ -1,51 +1,53 @@
 package com.sapuglha.tictactoe
 
-import android.app.Activity
 import android.app.Application
 import android.os.StrictMode
 import com.sapuglha.tictactoe.di.DaggerAppComponent
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
+import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
 
-class TttApp : Application(), HasActivityInjector {
+class TttApp : Application(), HasAndroidInjector {
     @Inject
-    lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+    internal lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+    override fun androidInjector() = dispatchingAndroidInjector
 
     override fun onCreate() {
         super.onCreate()
 
         DaggerAppComponent
-                .builder()
-                .application(this)
-                .build()
-                .inject(this);
+            .builder()
+            .application(this)
+            .build()
+            .inject(this);
 
         initialize()
     }
-
-    override fun activityInjector(): DispatchingAndroidInjector<Activity> = activityDispatchingAndroidInjector
 
     private fun initialize() {
 
         if (BuildConfig.DEBUG) {
             // strict mode for dev debug versions
-            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
                     .detectCustomSlowCalls()
 //                    .detectDiskReads() // incompatible with dataBinding
                     .detectDiskWrites()
                     .detectNetwork()
                     .penaltyLog()
                     .penaltyDeath()
-                    .build())
-            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
                     .detectLeakedClosableObjects()
                     .detectLeakedSqlLiteObjects()
                     .detectLeakedRegistrationObjects()
                     .penaltyLog()
                     .penaltyDeath()
-                    .build())
+                    .build()
+            )
         }
     }
 }
